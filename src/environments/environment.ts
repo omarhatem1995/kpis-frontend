d4123ff52f31e2bfcdec5de96e0f5b0c1f5024e3
O-Kortam <omar.kortam@cic.ae>
2026-07-19 19:32:24 +0300

    fix: switch to TCP probes, permit actuator health, update prod backend URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://omar-kortam-omar-space.hf.space/api/v1',
+  apiBaseUrl: "https://omar-kortam-omar-space.hf.space/api/v1",
   firebase: {
-    apiKey: 'AIzaSyA2FcFMUdasVtK5bl-Z1xtdiS28TaNIp20',
-    authDomain: 'cic-kpis.firebaseapp.com',
-    projectId: 'cic-kpis',
-    storageBucket: 'cic-kpis.firebasestorage.app',
-    messagingSenderId: '596669272965',
-    appId: '1:596669272965:web:72c66b135aac0f1db0aaec',
-    vapidKey: 'BDbVTI3JKXbuvIM80DUwfRqSRGvDZPHRh1m8tJUgFf8Dbb6Q28H1y8b-KO_yOfEb-oyWMTHQqGXJ0MDFWjteFg4'
-  }
+    apiKey: "AIzaSyA2FcFMUdasVtK5bl-Z1xtdiS28TaNIp20",
+    authDomain: "cic-kpis.firebaseapp.com",
+    projectId: "cic-kpis",
+    storageBucket: "cic-kpis.firebasestorage.app",
+    messagingSenderId: "596669272965",
+    appId: "1:596669272965:web:72c66b135aac0f1db0aaec",
+    vapidKey:
+      "BDbVTI3JKXbuvIM80DUwfRqSRGvDZPHRh1m8tJUgFf8Dbb6Q28H1y8b-KO_yOfEb-oyWMTHQqGXJ0MDFWjteFg4",
+  },
 };
